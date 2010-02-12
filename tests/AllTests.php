806fb6219c0c73287741f002852b2e140222a3b6
Daniel O'Connor <clockwerx@php.net>
2010-02-12 00:11:48 +0000

    SVN reorganisation / fixed tests
    git-svn-id: http://svn.php.net/repository/pear/packages/URI_Template/trunk@294940 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -38,7 +38,6 @@ if (!defined("PHPUnit_MAIN_METHOD")) {
     define("PHPUnit_MAIN_METHOD", "URI_Template_AllTests::main");
 }
 
-require_once "PHPUnit/Framework/TestSuite.php";
 require_once "PHPUnit/TextUI/TestRunner.php";
 
 require_once "URI/Template/TemplateTest.php";
